2daae0fecf798f8660704e2e9c77b2e514c9a9f8
hanwei <hanwei@xiaomi.com>
2021-04-25 20:12:46 +0800

    C6_1
    Signed-off-by: hanwei <hanwei@xiaomi.com>

---
+++ C6_1/app/src/main/java/zhaohad/glpath/c6_1/GLRender.java
@@ -0,0 +1,127 @@
+package zhaohad.glpath.c6_1;
+
+import android.content.Context;
+import android.opengl.GLES20;
+import android.opengl.GLES30;
+import android.opengl.GLSurfaceView;
+import android.opengl.Matrix;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+import zhaohad.glpath.c6_1.util.MatrixHelper;
+import zhaohad.glpath.c6_1.util.ShaderUtils;
+import zhaohad.glpath.c6_1.util.FileUtils;
+
+public class GLRender implements GLSurfaceView.Renderer {
+
+    private static final int BYTES_PER_FLOAT = 4;
+
+    private static final String A_POSITION = "a_Position";
+    private static final String A_COLOR = "a_Color";
+    private static final String U_MATRIX = "u_Matrix";
+
+    private static final int POSITION_COMPONENT_COUNT = 2;
+    private static final int COLOR_COMPONENT_COUNT = 3;
+
+    private static final int STRIDE = (POSITION_COMPONENT_COUNT + COLOR_COMPONENT_COUNT) * BYTES_PER_FLOAT;
+
+    private float[] mVertices;
+    private FloatBuffer mVerticesBuf;
+
+    private Context mContext;
+    private int mProgramId;
+    private int maPosition;
+    private int maColor;
+    private int muMatrix;
+    private float[] mProjectionMat = new float[4 * 4];
+    private float[] mModelMat = new float[4 * 4];
+
+    public GLRender(Context context) {
+        mContext = context;
+        mVertices = new float[] {
+                // Triangle Fan
+                 0f,    0f,   1f,   1f,   1f,
+                -0.5f, -0.8f, 0.7f, 0.7f, 0.7f,
+                 0.5f, -0.8f, 0.7f, 0.7f, 0.7f,
+                 0.5f,  0.8f, 0.7f, 0.7f, 0.7f,
+                -0.5f,  0.8f, 0.7f, 0.7f, 0.7f,
+                -0.5f, -0.8f, 0.7f, 0.7f, 0.7f,
+
+                // Line 1
+                -0.5f, 0f, 1f, 0f, 0f,
+                 0.5f, 0f, 1f, 0f, 0f,
+
+                // Mallets
+                0f, -0.4f, 0f, 0f, 1f,
+                0f,  0.4f, 1f, 0f, 0f
+        };
+
+        mVerticesBuf = ByteBuffer
+                .allocateDirect(mVertices.length * BYTES_PER_FLOAT)
+                .order(ByteOrder.nativeOrder())
+                .asFloatBuffer();
+        mVerticesBuf.put(mVertices);
+    }
+
+    @Override
+    public void onSurfaceCreated(GL10 gl10, EGLConfig eglConfig) {
+        String vShader = FileUtils.readFromRaw(mContext, R.raw.simple_vertex_shader);
+        String fShader = FileUtils.readFromRaw(mContext, R.raw.simple_fragment_shader);
+
+        // 调用glCreateShader之前必须先调用mGLSurfaceView.setEGLContextClientVersion(2);
+        // 否则glCreateShader会出现 Fatal signal 11 (SIGSEGV)的native crash
+        int vShaderId = ShaderUtils.compileVShader(vShader);
+        int fShaderId = ShaderUtils.compileFShader(fShader);
+
+        mProgramId = ShaderUtils.linkProgram(vShaderId, fShaderId);
+
+        ShaderUtils.validateProgram(mProgramId);
+
+        GLES30.glUseProgram(mProgramId);
+
+        maPosition = GLES30.glGetAttribLocation(mProgramId, A_POSITION);
+        maColor = GLES30.glGetAttribLocation(mProgramId, A_COLOR);
+        muMatrix = GLES30.glGetUniformLocation(mProgramId, U_MATRIX);
+
+        mVerticesBuf.position(0);
+        GLES30.glVertexAttribPointer(maPosition, POSITION_COMPONENT_COUNT, GLES30.GL_FLOAT, false, STRIDE, mVerticesBuf);
+        GLES30.glEnableVertexAttribArray(maPosition);
+
+        mVerticesBuf.position(POSITION_COMPONENT_COUNT);
+        GLES30.glVertexAttribPointer(maColor, COLOR_COMPONENT_COUNT, GLES30.GL_FLOAT, false, STRIDE, mVerticesBuf);
+        GLES30.glEnableVertexAttribArray(maColor);
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl10, int width, int height) {
+        GLES30.glViewport(0, 0, width, height);
+
+        MatrixHelper.perspectiveM(mProjectionMat, 45, (float) width / (float) height, 1, 10);
+
+        Matrix.setIdentityM(mModelMat, 0);
+        Matrix.translateM(mModelMat, 0, 0, 0, -2.5f);
+        Matrix.rotateM(mModelMat, 0, -60, 1, 0, 0);
+
+        float[] tmpMat = new float[4 * 4];
+        Matrix.multiplyMM(tmpMat, 0, mProjectionMat, 0, mModelMat, 0);
+        System.arraycopy(tmpMat, 0, mProjectionMat, 0, tmpMat.length);
+
+        GLES30.glUniformMatrix4fv(muMatrix, 1, false, mProjectionMat, 0);
+    }
+
+    @Override
+    public void onDrawFrame(GL10 gl10) {
+        GLES30.glClear(GLES30.GL_COLOR_BUFFER_BIT);
+
+        GLES30.glDrawArrays(GLES20.GL_TRIANGLE_FAN, 0, 6);
+
+        GLES30.glDrawArrays(GLES20.GL_LINES, 6, 2);
+
+        GLES30.glDrawArrays(GLES20.GL_POINTS, 8, 2);
+    }
+}
---
+++ C6_1/app/src/main/java/zhaohad/glpath/c6_1/util/ShaderUtils.java
@@ -0,0 +1,72 @@
+package zhaohad.glpath.c6_1.util;
+
+import android.opengl.GLES20;
+import android.opengl.GLES30;
+import android.util.Log;
+
+public class ShaderUtils {
+    public static int compileVShader(String code) {
+        return compileShader(GLES30.GL_VERTEX_SHADER, code);
+    }
+
+    public static int compileFShader(String code) {
+        return compileShader(GLES30.GL_FRAGMENT_SHADER, code);
+    }
+
+    private static int compileShader(int type, String code) {
+        int sId = GLES30.glCreateShader(type);
+
+        if (sId == 0) {
+            Log.e("hanwei", "shader create failed");
+            return 0;
+        }
+
+        GLES30.glShaderSource(sId, code);
+        GLES30.glCompileShader(sId);
+
+        int[] compileRes = new int[1];
+        GLES30.glGetShaderiv(sId, GLES20.GL_COMPILE_STATUS, compileRes, 0);
+        Log.e("hanwei", "compiling: " + GLES30.glGetShaderInfoLog(sId));
+
+        if (compileRes[0] == 0) {
+            GLES30.glDeleteShader(sId);
+            Log.e("hanwei", "shaer compile failed");
+            return 0;
+        }
+
+        return sId;
+    }
+
+    public static int linkProgram(int vShaderId, int fShaderId) {
+        int pId = GLES30.glCreateProgram();
+        if (pId == 0) {
+            Log.e("hanwei", "Program create failed");
+        }
+
+        GLES30.glAttachShader(pId, vShaderId);
+        GLES30.glAttachShader(pId, fShaderId);
+
+        GLES30.glLinkProgram(pId);
+
+        int[] linkRes = new int[1];
+        GLES30.glGetProgramiv(pId, GLES20.GL_LINK_STATUS, linkRes, 0);
+        Log.e("hanwei", "Linking: " + GLES30.glGetProgramInfoLog(pId));
+
+        if (linkRes[0] == 0) {
+            GLES30.glDeleteProgram(pId);
+            Log.e("hanwei", "Program link failed");
+            return 0;
+        }
+
+        return pId;
+    }
+
+    public static boolean validateProgram(int pId) {
+        GLES30.glValidateProgram(pId);
+        int[] vStatus = new int[1];
+        GLES30.glGetProgramiv(pId, GLES20.GL_VALIDATE_STATUS, vStatus, 0);
+        Log.e("hanwei", "validateProgram status: " + vStatus[0]);
+
+        return vStatus[0] != 0;
+    }
+}
